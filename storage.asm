96902ae0c161d86ae414a8ddcecbd8d6ae9f1234
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-12-01 21:49:48 -0300

    Add verifications to add action and add print list
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- storage.asm
+++ storage.asm
@@ -1,43 +1,47 @@
 .data
-	list_size_prompt: .asciiz "Tell me the list size: "
-	menu_prompt: .asciiz "-- MIPS List Storage  --\n\n 1 - Add Elements \n 2 - Recover Element \n 3 - Print List \n 4 - Remove Element \n 5 - Exit \n\n >> "
-	invalid_option_prompt: .asciiz "\n\nInvalid Option! Try again.\n\n"
-	exit_prompt: .asciiz "\n\nFinishing the application!\n\n"
-	add_element_prompt: .asciiz "Element to add >> "
-	recover_element_prompt: .asciiz "Element position >> "
-	print_list_prompt: .asciiz "\n\n-- List --\n\n"
+	list_size_prompt: .asciiz "\nMax list size >> "
+	menu_prompt: .asciiz "\n-- MIPS List Storage  --\n\n 1 - Add Elements \n 2 - Recover Element \n 3 - Print List \n 4 - Remove Element \n 5 - Exit \n\n>> "
+	invalid_option_prompt: .asciiz "\nInvalid Option! Try again.\n"
+	exit_prompt: .asciiz "\nFinishing the application!\n"
+	add_element_prompt: .asciiz "\nElement to add >> "
+	full_list_error_prompt: .asciiz "\n\nThe list is full! Action aborted.\n\n"
+	recover_element_prompt: .asciiz "\nElement position >> "
+	print_list_prompt: .asciiz "\n-- List --\n"
+	new_line: .asciiz "\n"
 	
-	list_size: .word -1
+	max_list_size: .word -1
+	current_list_size: .word 0
+	list: .word
 	menu_selection: .word 999
+	
 .text
-
-main:
 	la 	$a0, list_size_prompt
 	li 	$v0, 4
 	syscall
 	
 	li 	$v0, 5
 	syscall
-	sw 	$v0, list_size
+	sw 	$v0, max_list_size
 	
-	loop:		
-		jal 	show_menu_and_load_selection
+loop:		
+	jal 	show_menu_and_load_selection
 		
-		la 	$t0, menu_selection
-		lw 	$t0, ($t0)
+	la 	$t0, menu_selection
+	lw 	$t0, ($t0)
 	
-		beq	$t0, 1, add_element
-		beq	$t0, 2, recover_element
-		beq	$t0, 3, print_list
-		beq	$t0, 4, delete_element
-		beq	$t0, 5, end_loop
+	beq	$t0, 1, add_element
+	beq	$t0, 2, recover_element
+	beq	$t0, 3, print_list
+	beq	$t0, 4, delete_element
+	beq	$t0, 5, end_loop
 		
-		la 	$a0, invalid_option_prompt
-		li 	$v0, 4
-		syscall
+	la 	$a0, invalid_option_prompt
+	li 	$v0, 4
+	syscall
 	
-		j 	loop
-	end_loop:
+	j 	loop
+	
+end_loop:
 	
 	j	exit
 	
@@ -53,6 +57,13 @@ show_menu_and_load_selection:
 	jr 	$ra
 	
 add_element:
+	la	$s0, current_list_size
+	lw 	$s0, ($s0)
+	la	$s1, max_list_size
+	lw 	$s1, ($s1)
+	
+	beq	$s0, $s1, add_element_error
+	
 	la	$a0, add_element_prompt
 	li	$v0, 4
 	syscall
@@ -60,6 +71,16 @@ add_element:
 	li 	$v0, 5
 	syscall
 	
+	la	$s2, list
+	sw 	$v0, 8($s2)
+	
+	j	loop
+	
+add_element_error:
+	la 	$a0, full_list_error_prompt
+	li 	$v0, 4
+	syscall
+		
 	j	loop
 
 recover_element:
@@ -77,7 +98,27 @@ print_list:
 	li	$v0, 4
 	syscall
 	
-	j	loop
+	la 	$s0, list
+	li 	$s1, 0
+	la	$s2, current_list_size
+	lw 	$s2, ($s2)
+	
+for:
+	beq 	$s1, $s2, loop
+	li 	$t0, 4
+
+	mul 	$t0, $t0, $s1
+	
+	add 	$t0, $t0, $s0
+	lw 	$a0, 0($t0)	
+	jal 	print_element
+	
+	la 	$a0, new_line
+	li 	$v0, 4
+	syscall	
+	
+	addi 	$s1, $s1, 1
+	j 	for
 	
 delete_element:
 	la	$a0, recover_element_prompt
